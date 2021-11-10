ea97279a6d02918f379b91f4f5fa103b97d73838
NicoBWin <nbustelow@gmail.com>
2021-11-10 10:10:21 -0300

    Update ROM_SerialAddress.v

--- ROM_SerialAddress.v
+++ ROM_SerialAddress.v
@@ -1,36 +1,31 @@
 module Memory4x4( 
-
 	input wire[3:0] ADDR, 
-
 	input wire CS,
-	
 	input wire start,
-
 	output reg OUT
-
 );
 
-reg [3:0]mem[15:0];
+	reg [3:0]mem[15:0];
 
-integer i;
-integer j=0;	
+	integer i;
+	integer j=0;	
 
-initial begin
-	for (i = 0; i < 16; i = i+1)
-		mem[i] = i;
-end
-
-LSOSC u_HSOSC(.CLKLFPU(1), .CLKLFEN(1), .CLKLF(int_osc));
-
-always @(posedge int_osc)
-	if (CS && start)
-	begin
-		OUT = mem[ADDR][j];
-		if(j<4)
-			j=j+1;
-		else
-			j=0;
+	initial begin
+		for (i = 0; i < 16; i = i+1)
+			mem[i] = i;
 	end
-	else
-		OUT = 1'bz;
+
+	LSOSC u_HSOSC(.CLKLFPU(1), .CLKLFEN(1), .CLKLF(int_osc));
+
+	always @(posedge int_osc)
+		if (CS && start) 
+		begin
+			OUT = mem[ADDR][j];
+			if(j<4)
+				j=j+1;
+			else
+				j=0;
+		end
+		else
+			OUT = 1'bz;
 endmodule
